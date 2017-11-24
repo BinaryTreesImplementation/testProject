dd3f134a2dd90f7bd21e9fa30f167b0f709e1e2c
MaximSurovtsev <gr4m0zeka@gmail.com>
2017-11-24 20:07:45 +0300

    upd tests

--- tests/splay.cpp
+++ tests/splay.cpp
@@ -17,7 +17,7 @@ Insert left child
 	  7				  10
 */
 
-TEST_CASE("Insert left child", "[ilch]") {
+TEST_CASE("Inserting left child", "[ilch]") {
    SplayTree<int> testSplayTree;
    testSplayTree.insert(10);
    testSplayTree.insert(7);
@@ -35,7 +35,7 @@ Insert right child
 	  	   15		10
 */
 
-TEST_CASE("Insert right child", "[irch]") {
+TEST_CASE("Inserting right child", "[irch]") {
    SplayTree<int> testSplayTree;
    testSplayTree.insert(10);
    testSplayTree.insert(15);
@@ -44,3 +44,55 @@ TEST_CASE("Insert right child", "[irch]") {
    REQUIRE(testSplayTree.getCount() == 2);
    REQUIRE(testSplayTree.getKey(testSplayTree.getRoot()) == 15 );
 }
+
+
+/* 
+Inserting right child with left brother
+
+	10	  	     15   		  15		     20
+	  \   ->    /     ->     /  \    ->     /
+	   15 	  10		   10    20		   15
+	   									  /	
+	  		  	 						 10
+*/
+
+TEST_CASE("Inserting right child with left brother", "[ilch]") {
+   SplayTree<int> testSplayTree;
+   testSplayTree.insert(10);
+   testSplayTree.insert(15);
+   testSplayTree.insert(20);      
+   REQUIRE(testSplayTree.search(10) == 1);
+   REQUIRE(testSplayTree.search(7) == 1);      
+   REQUIRE(testSplayTree.search(7) == 1);
+   REQUIRE(testSplayTree.getCount() == 3);
+   REQUIRE(testSplayTree.getKey(testSplayTree.getRoot()) == 20 );
+}
+
+/* 
+Inserting left child with right brother
+
+	15	  	  10   		  	10	              20
+	/   ->      \    ->       \       ->     /
+  10    	  	 15		       15		    15
+	   							 \ 		   /	
+	  		  	 				  20	  10
+*/
+
+
+TEST_CASE("Inserting right child with right parent", "[ilch]") {
+   SplayTree<int> testSplayTree;
+   testSplayTree.insert(15);
+   testSplayTree.insert(10);
+   testSplayTree.insert(20);      
+   REQUIRE(testSplayTree.search(10) == 1);
+   REQUIRE(testSplayTree.search(7) == 1);      
+   REQUIRE(testSplayTree.search(7) == 1);
+   REQUIRE(testSplayTree.getCount() == 3);
+   REQUIRE(testSplayTree.getKey(testSplayTree.getRoot()) == 20 );
+}
+
+
+
+
+
+
